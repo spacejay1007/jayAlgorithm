22ccc0fb9e87672f234c13984ca027e762c585ee
Jay <spacejay1007@gmail.com>
2022-12-22 01:21:11 +0900

    test

--- assignProblem/subMission.ts
+++ assignProblem/subMission.ts
@@ -2,4 +2,8 @@
 // 이 파일안 data array의 각 row data는 아래 형식대로 되어있는데 dirId 는 고유값이고, dirPid 는 부모의 id값이다.
 // 부모의 id값이 -1 이면  Root 이고 있으면 부모의 children에 들어가야 한다.
 // 올바른 데이터 매핑 구조 로직을 구현하면 됩니다
+import sub from "./subMissionBefore.json"
 
+export const treeFunc = () => {
+    console.log(sub)
+}
